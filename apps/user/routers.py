ce55e9ff80e9f04c95588c7405e7bc83423a22bd
seventh_attempt <asd99304@gmail.com>
2019-09-19 17:27:00 +0300

    added user authentication

--- apps/user/routers.py
+++ apps/user/routers.py
@@ -7,3 +7,4 @@ from apps.user.views.playlist import PlaylistView
 router = routers.DefaultRouter()
 router.register('user', UserView)
 router.register('playlist', PlaylistView)
+router.register('registr_rest', UserRegistrationView)
